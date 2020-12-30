69a8565e384339277162c285953fa068522d1627
Ascetics <mingkun.ling@qq.com>
2020-12-30 11:21:01 +0800

    add bullet controled disappear

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,12 @@ class AlienInvasion():
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # 清除消失的子弹
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
             self._update_screen()
 
     def _check_events(self):
