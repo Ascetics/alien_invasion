f839318100f6fb9cbb553297a24585640cf4cd62
Ascetics <mingkun.ling@qq.com>
2020-12-31 10:53:51 +0800

    draw a lot of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -63,11 +63,37 @@ class AlienInvasion():
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _create_alien(self, alien_number, row_number):
+        """
+        创建alien并放入alien的Group
+        """
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number 
+        alien.rect.x = alien.x        
+        alien.y = alien_height + 2 * alien_height * row_number
+        alien.rect.y = alien.y
+        self.aliens.add(alien) 
+
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
-        pass
+        alien_width, alien_height = alien.rect.size
 
+        # 横向可以放多少个alien
+        available_space_x = self.settings.screen_width - alien_width
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # 纵向可以放多少行alien
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+            (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        # 创建一系列alien
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+                     
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
